9abca920bc66c94f673e3685ed41afa8dcb70f18
mozeXezom <79330736+mozeXezom@users.noreply.github.com>
2023-07-19 00:27:58 +0300

    Initial Commit

---
+++ Secrets/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Secrets
+//
+//  Created by mozeX on 19.07.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
